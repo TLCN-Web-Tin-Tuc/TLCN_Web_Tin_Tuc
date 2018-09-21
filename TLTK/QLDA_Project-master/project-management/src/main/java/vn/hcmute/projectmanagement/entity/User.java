e4b5abcdcdfa37ab02dad2a765dc357866110071
tri291997 <xuantri97@gmail.com>
2018-09-21 14:51:16 +0700

    Create TLTK

---
+++ TLTK/QLDA_Project-master/project-management/src/main/java/vn/hcmute/projectmanagement/entity/User.java
@@ -0,0 +1,35 @@
+package vn.hcmute.projectmanagement.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+
+import javax.persistence.*;
+import java.util.BitSet;
+import java.util.Set;
+
+@Entity(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String username;
+    private String password;
+    private int status;
+
+    @OneToOne(mappedBy = "user",fetch = FetchType.LAZY)
+    private Person person;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id",referencedColumnName = "id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id",referencedColumnName = "id")
+    )
+    private Set<Role> roles;
+}
