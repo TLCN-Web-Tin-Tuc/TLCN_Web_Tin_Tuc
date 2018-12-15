34248e807a9fa282711bb0a9080fc178f313ee4f
Hoang Long <hoanglong03061997@gmail.com>
2018-12-15 08:45:11 +0700

    - mod service

--- Front-end/src/app/_service/mod_service/mod-service.service.ts
+++ Front-end/src/app/_service/mod_service/mod-service.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { Item } from '../../_entity/item';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,4 +11,9 @@ export class ModServiceService {
 
   context = environment.base_mod_url
   constructor(private http: HttpClient) { }
+
+  // login(guest: Item) : Observable<any>{    
+  //   return this.http.post(`${this.context}/api/v1/mod/login/${guest.email}/${guest.password}`,{observe:`response`});
+  // }
+
 }
